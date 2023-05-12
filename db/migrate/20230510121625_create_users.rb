3259fa011ab76132847c0cc21b00cb98d182ed50
benessilfie <bquansah007@gmail.com>
2023-05-12 15:20:08 +0000

    User and note migrations

---
+++ db/migrate/20230510121625_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.integer :role, default: 1 # default role is member
+      t.string :auth_token
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20230510172115_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notes do |t|
+      t.string :title
+      t.text :content
+      t.boolean :published
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
